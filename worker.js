9706f597d434f7793ef392f336eca2cad94dfadf
wanjawischmeier <69650445+wanjawischmeier@users.noreply.github.com>
2025-11-28 18:39:16 +0100

    Update worker.js for use with vue

--- worker.js
+++ worker.js
@@ -1,13 +1,19 @@
-'use strict';
+import { Buffer } from 'buffer/';
+if (typeof globalThis.Buffer === 'undefined') {
+  globalThis.Buffer = Buffer;
+}
 
-const {init, WASI} = require('@wasmer/wasi');
+import { init, WASI } from '@wasmer/wasi';
 
 const module_cache = {};
 let wsmInitialized = false;
 
 addEventListener('message', (event) => {
-  executeEspresso(event.data.input, event.data.args)
-      .then(postMessage);
+  // Expect message format: { input, args, wasmPath }
+  const { input, args, wasmPath } = event.data || {}
+  executeEspresso(input ?? '', args ?? [], wasmPath)
+    .then((result) => postMessage(result))
+    .catch((err) => postMessage({ exitCode: 1, stdout: '', stderr: String(err) }))
 });
 
 /**
@@ -16,27 +22,29 @@ addEventListener('message', (event) => {
  * @return {Promise<{exitCode: number, stdout: string, stderr: string}>}
  */
 async function executeEspresso(input, args) {
-  if(!wsmInitialized) {
-    await init();
-    wsmInitialized = true;
+  if (!wsmInitialized) {
+    await init()
+    wsmInitialized = true
   }
 
-  const wasi = new WASI({env: {}, args: ['espresso', ...args, '/input.esp']});
+  const wasi = new WASI({ env: {}, args: ['espresso', ...(args || []), '/input.esp'] })
 
-  const file = wasi.fs.open('/input.esp', {read: true, write: true, create: true});
-  file.writeString(input);
-  file.flush();
+  const file = wasi.fs.open('/input.esp', { read: true, write: true, create: true })
+  file.writeString(input ?? '')
+  file.flush()
 
-  await instantiateModule(wasi, 'espresso.wasm');
+  // Use provided wasmPath or fall back to default location
+  const wasmToLoad = typeof wasmPath === 'string' && wasmPath.length > 0 ? wasmPath : 'espresso.wasm'
+  await instantiateModule(wasi, wasmToLoad)
 
-  console.debug('Running Espresso...');
-  const exitCode = wasi.start();
-  console.debug('Espresso finished with exit code', exitCode);
+  console.debug('Running Espresso...')
+  const exitCode = wasi.start()
+  console.debug('Espresso finished with exit code', exitCode)
   return {
     exitCode,
     stdout: wasi.getStdoutString(),
-    stderr: wasi.getStderrString()
-  };
+    stderr: wasi.getStderrString(),
+  }
 }
 
 /**
@@ -64,5 +72,3 @@ async function instantiateModule(wasi, moduleName) {
 
   await wasi.instantiate(instance, {});
 }
-
-exports.executeEspresso = executeEspresso;
